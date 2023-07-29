479de610d7533f615fb42c2ee1a6a09e4d38f36f
Iuga Bogdan <iugabogdan13@gmail.com>
2023-07-30 00:15:22 +0300

    aaa

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,13 +1,8 @@
 import React, { useEffect, useState, useRef } from 'react';
 import S from './bubble.module.css';
 
-// Helper function to get random size within a range
-const getRandomSize = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
 // Helper function to get random position for the bubble
-const getRandomPosition = (bubbleSize, takenPositions, maxSize, wrapperWidth, wrapperHeight, gap) => {
+const getRandomPosition = (bubbleSize, wrapperWidth, wrapperHeight, gap) => {
     const xRange = wrapperWidth - bubbleSize - gap * 2;
     const yRange = wrapperHeight - bubbleSize - gap * 2;
 
@@ -21,66 +16,56 @@ const getRandomPosition = (bubbleSize, takenPositions, maxSize, wrapperWidth, wr
     };
 };
 
+// Function to determine bubble size based on price change
+const getSize = (token) => {
+    const absPriceChange = Math.abs(parseFloat(token.price_change_percentage));
+    const size = Math.max(30, absPriceChange * 5); // Modify this as needed
+    return size;
+};
+
+// Function to check if two bubbles are colliding
+const isColliding = (a, b, bubbleSize) => {
+    return Math.hypot(a.x - b.x, a.y - b.y) < bubbleSize;
+};
+
 const Bubble = ({ tokens }) => {
     const wrapper = useRef(null);
-    const bubbles = tokens.length;
     const gap = 10; // gap between bubbles
-    const minSize = 30; // Minimum size of the bubble
-    const maxSize = 100; // Maximum size of the bubble
+
+    const [positions, setPositions] = useState(Array(tokens.length));
+    const [placed, setPlaced] = useState(Array(tokens.length).fill(false));
 
     useEffect(() => {
-        // Accessing the height of the div after the component has rendered
-        if (wrapper.current) {
-            const height = wrapper.current.clientHeight;
-        }
-    }, []);
-
-    const [positions, setPositions] = useState([]);
-
-    useEffect(() => {
-        const generateBubblePositions = () => {
-            let maxSize = minSize;
-
+        const placeBubble = (bubbleIndex) => {
             const wrapperWidth = wrapper.current.clientWidth;
             const wrapperHeight = wrapper.current.clientHeight;
 
-            const tempPositions = [];
-            for (let i = 0; i < bubbles; i++) {
-                let position;
-                let colliding;
-                let bubbleSize;
+            const bubbleSize = getSize(tokens[bubbleIndex]);
 
-                do {
-                    bubbleSize = getRandomSize(minSize, maxSize);
-                    position = getRandomPosition(bubbleSize, tempPositions, maxSize, wrapperWidth, wrapperHeight, gap);
-                    colliding = tempPositions.some((pos) => isColliding(pos, position, bubbleSize));
-                } while (colliding);
+            let position;
+            let colliding;
+            do {
+                position = getRandomPosition(bubbleSize, wrapperWidth, wrapperHeight, gap);
+                colliding = positions.some((pos, i) => placed[i] && isColliding(pos, position, bubbleSize));
+            } while (colliding);
 
-                tempPositions.push(position);
-                maxSize += gap; // Increment maxSize to allow larger bubbles in subsequent iterations
-            }
+            setPositions(prevPositions => {
+                const updatedPositions = [...prevPositions];
+                updatedPositions[bubbleIndex] = position;
+                return updatedPositions;
+            });
 
-            setPositions(tempPositions);
+            setPlaced(prevPlaced => {
+                const updatedPlaced = [...prevPlaced];
+                updatedPlaced[bubbleIndex] = true;
+                return updatedPlaced;
+            });
         };
 
-        // Call the function to generate positions when the component mounts
-        generateBubblePositions();
-
-        // Optionally, you can add an event listener to recalculate positions on window resize
-        const handleResize = () => {
-            generateBubblePositions();
-        };
-        window.addEventListener('resize', handleResize);
-
-        // Clean up the event listener when the component unmounts
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [bubbles]);
-
-    const isColliding = (a, b, bubbleSize) => {
-        return Math.hypot(a.x - b.x, a.y - b.y) < bubbleSize + gap;
-    };
+        for(let i = 0; i < tokens.length; i++) {
+            placeBubble(i);
+        }
+    }, [tokens]);
 
     const getClassName = (priceChange) => {
         return priceChange > 0 ? S.bubbleGreen : S.bubbleRed;
@@ -127,18 +112,18 @@ const Bubble = ({ tokens }) => {
                             src={`https://media.elrond.com/tokens/asset/${tokens[i].token_id}/logo.svg`}
                             style={{ width: '70%', height: '70%' }}
                         />
-          {/*              {shouldShowText && (
+                        {shouldShowText && (
                             <span
                                 style={{ fontWeight: 'bold', marginBottom: '5px', fontSize: '14px', lineHeight: '1.2' }}
                             >
-                {getTokenName(tokens[i].token_id)}
-              </span>
+                                {getTokenName(tokens[i].token_id)}
+                            </span>
                         )}
                         {shouldShowText && (
                             <span style={{ fontSize: '12px', lineHeight: '1.2' }}>
-                {parseFloat(tokens[i].price_change_percentage).toFixed(2)}%
-              </span>
-                        )}*/}
+                                {parseFloat(tokens[i].price_change_percentage).toFixed(2)}%
+                            </span>
+                        )}
                     </div>
                 );
             })}
