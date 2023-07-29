14f7bd34515424c26e906c4b7c8b15a771999648
Andrei Sabou <andrei.sabou@metro.digital>
2023-07-29 23:01:17 +0300

    bubbles with img

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -1,107 +1,112 @@
 import React, { useEffect, useState, useRef } from 'react';
 import S from './bubble.module.css';
 
-const Bubble = ({tokens}) => {
-        const wrapper = useRef(null);
-        const bubbles = tokens.length;
-        const gap = 10; // gap between bubbles
-        const minSize = 30; // Minimum size of the bubble
-        const maxSize = 100; // Maximum size of the bubble
+// Helper function to get random size within a range
+const getRandomSize = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-        useEffect(() => {
-            // Accessing the height of the div after the component has rendered
-            if (wrapper.current) {
-                const height = wrapper.current.clientHeight;
-            }
-        }, []);
+// Helper function to get random position for the bubble
+const getRandomPosition = (bubbleSize, takenPositions, maxSize, wrapperWidth, wrapperHeight, gap) => {
+    const xRange = wrapperWidth - bubbleSize - gap * 2;
+    const yRange = wrapperHeight - bubbleSize - gap * 2;
 
-        const getRandomSize = (min, max) => {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        };
+    const x = gap + Math.random() * xRange;
+    const y = gap + Math.random() * yRange;
 
-        const getRandomPosition = (bubbleSize, takenPositions, maxSize) => {
+    return {
+        x,
+        y,
+        size: bubbleSize,
+    };
+};
+
+const Bubble = ({ tokens }) => {
+    const wrapper = useRef(null);
+    const bubbles = tokens.length;
+    const gap = 10; // gap between bubbles
+    const minSize = 30; // Minimum size of the bubble
+    const maxSize = 100; // Maximum size of the bubble
+
+    useEffect(() => {
+        // Accessing the height of the div after the component has rendered
+        if (wrapper.current) {
+            const height = wrapper.current.clientHeight;
+        }
+    }, []);
+
+    const [positions, setPositions] = useState([]);
+
+    useEffect(() => {
+        const generateBubblePositions = () => {
+            let maxSize = minSize;
+
             const wrapperWidth = wrapper.current.clientWidth;
             const wrapperHeight = wrapper.current.clientHeight;
 
-            const xRange = wrapperWidth - bubbleSize - gap * 2;
-            const yRange = wrapperHeight - bubbleSize - gap * 2;
+            const tempPositions = [];
+            for (let i = 0; i < bubbles; i++) {
+                let position;
+                let colliding;
+                let bubbleSize;
 
-            const x = gap + Math.random() * xRange;
-            const y = gap + Math.random() * yRange;
+                do {
+                    bubbleSize = getRandomSize(minSize, maxSize);
+                    position = getRandomPosition(bubbleSize, tempPositions, maxSize, wrapperWidth, wrapperHeight, gap);
+                    colliding = tempPositions.some((pos) => isColliding(pos, position, bubbleSize));
+                } while (colliding);
 
-            return {
-                x,
-                y,
-                size: bubbleSize,
-            };
+                tempPositions.push(position);
+                maxSize += gap; // Increment maxSize to allow larger bubbles in subsequent iterations
+            }
+
+            setPositions(tempPositions);
         };
 
-        const isColliding = (a, b, bubbleSize) => {
-            return Math.hypot(a.x - b.x, a.y - b.y) < bubbleSize + gap;
-        };
+        // Call the function to generate positions when the component mounts
+        generateBubblePositions();
 
-        const [positions, setPositions] = useState([]);
-
-        useEffect(() => {
-            const generateBubblePositions = () => {
-                let maxSize = minSize;
-
-                const tempPositions = [];
-                for (let i = 0; i < bubbles; i++) {
-                    let position;
-                    let colliding;
-                    let bubbleSize;
-
-                    do {
-                        bubbleSize = getRandomSize(minSize, maxSize);
-                        position = getRandomPosition(bubbleSize, tempPositions, maxSize);
-                        colliding = tempPositions.some((pos) => isColliding(pos, position, bubbleSize));
-                    } while (colliding);
-
-                    tempPositions.push(position);
-                    maxSize += gap; // Increment maxSize to allow larger bubbles in subsequent iterations
-                }
-
-                setPositions(tempPositions);
-            };
-
-            // Call the function to generate positions when the component mounts
+        // Optionally, you can add an event listener to recalculate positions on window resize
+        const handleResize = () => {
             generateBubblePositions();
-
-            // Optionally, you can add an event listener to recalculate positions on window resize
-            const handleResize = () => {
-                generateBubblePositions();
-            };
-            window.addEventListener('resize', handleResize);
-
-            // Clean up the event listener when the component unmounts
-            return () => {
-                window.removeEventListener('resize', handleResize);
-            };
-        }, [bubbles]);
-
-        const getClassName = (priceChange) => {
-            return priceChange > 0 ? S.bubbleGreen : S.bubbleRed;
         };
+        window.addEventListener('resize', handleResize);
 
-        const getTokenName = (token) => {
-            const separator = token.indexOf('-')
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [bubbles]);
 
-            return token.slice(0, separator);
-        }
+    const isColliding = (a, b, bubbleSize) => {
+        return Math.hypot(a.x - b.x, a.y - b.y) < bubbleSize + gap;
+    };
 
-        return (
-            <div
-                className={'Wrapper'}
-                ref={wrapper}
-                style={{
-                    height: '500px',
-                    width: '100%',
-                    position: 'relative',
-                    overflow: 'hidden',
-                }}
-            >
-                {positions.map((position, i) => (
+    const getClassName = (priceChange) => {
+        return priceChange > 0 ? S.bubbleGreen : S.bubbleRed;
+    };
+
+    const getTokenName = (token) => {
+        const separator = token.indexOf('-');
+        return token.slice(0, separator);
+    };
+
+    return (
+        <div
+            className={'Wrapper'}
+            ref={wrapper}
+            style={{
+                height: '500px',
+                width: '100%',
+                position: 'relative',
+                overflow: 'hidden',
+            }}
+        >
+            {positions.map((position, i) => {
+                const absPriceChange = Math.abs(parseFloat(tokens[i].price_change_percentage));
+                const shouldShowText = absPriceChange > 0.01;
+
+                return (
                     <div
                         key={i}
                         style={{
@@ -118,33 +123,27 @@ const Bubble = ({tokens}) => {
                         }}
                         className={`${S.bubble} ${getClassName(parseFloat(tokens[i].price_change_percentage))}`}
                     >
-                        <img src={`https://media.elrond.com/tokens/asset/${tokens[i].token_id}/logo.svg`}
-                             height={30}
-                             width={30}
+                        <img
+                            src={`https://media.elrond.com/tokens/asset/${tokens[i].token_id}/logo.svg`}
+                            style={{ width: '70%', height: '70%' }}
                         />
-                        {
-                            Math.abs(parseFloat(tokens[i].price_change_percentage)) > 0.01 && (
-                                <span
-                                    style={{fontWeight: 'bold', marginBottom: '5px', fontSize: '14px', lineHeight: '1.2'}}>
-            {getTokenName(tokens[i].token_id)}
-          </span>
-                            )
-                        }
-                        {
-                            Math.abs(parseFloat(tokens[i].price_change_percentage)) > 0.01 && (
-                                <span style={{
-                                    fontSize: '12px',
-                                    lineHeight: '1.2'
-                                }}>
-                                {parseFloat(tokens[i].price_change_percentage).toFixed(2)}%
-                            </span>
-                            )
-                        }
+          {/*              {shouldShowText && (
+                            <span
+                                style={{ fontWeight: 'bold', marginBottom: '5px', fontSize: '14px', lineHeight: '1.2' }}
+                            >
+                {getTokenName(tokens[i].token_id)}
+              </span>
+                        )}
+                        {shouldShowText && (
+                            <span style={{ fontSize: '12px', lineHeight: '1.2' }}>
+                {parseFloat(tokens[i].price_change_percentage).toFixed(2)}%
+              </span>
+                        )}*/}
                     </div>
-                ))}
-            </div>
-        );
-    }
-;
+                );
+            })}
+        </div>
+    );
+};
 
 export default Bubble;
