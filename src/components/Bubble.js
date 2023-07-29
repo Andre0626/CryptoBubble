cd2b0182f6c10e6d0b2e18baa241b2ad165ded95
Andrei Sabou <andrei.sabou@metro.digital>
2023-07-29 21:53:01 +0300

    Space between bubbles

--- src/components/Bubble.js
+++ src/components/Bubble.js
@@ -4,10 +4,11 @@ import S from './bubble.module.css';
 
 const Bubble = () => {
     const wrapper = useRef(null);
-    const bubbles = 100;
+    const bubbles = 50;
     const gap = 10; // gap between bubbles
     const minSize = 30; // Minimum size of the bubble
     const maxSize = 100; // Maximum size of the bubble
+    const gridSize = 20; // Size of grid cells for positioning
 
     useEffect(() => {
         // Accessing the height of the div after the component has rendered
@@ -37,14 +38,14 @@ const Bubble = () => {
         };
     };
 
-    const isColliding = (a, b, bubbleSize) => {
-        return Math.hypot(a.x - b.x, a.y - b.y) < bubbleSize + gap;
-    };
-
     const [positions, setPositions] = useState([]);
 
     useEffect(() => {
         const generateBubblePositions = () => {
+            const grid = new Array(Math.ceil(wrapper.current.clientWidth / gridSize))
+                .fill(null)
+                .map(() => new Array(Math.ceil(wrapper.current.clientHeight / gridSize)).fill(false));
+
             const tempPositions = [];
             for (let i = 0; i < bubbles; i++) {
                 let position;
@@ -54,13 +55,46 @@ const Bubble = () => {
                 do {
                     bubbleSize = getRandomSize(minSize, maxSize);
                     position = getRandomPosition(bubbleSize);
-                    colliding = tempPositions.some((pos) => isColliding(pos, position, bubbleSize));
+                    colliding = isCollidingWithGrid(grid, position, bubbleSize);
                 } while (colliding);
 
+                markGridOccupied(grid, position, bubbleSize);
                 tempPositions.push(position);
             }
 
             setPositions(tempPositions);
+        };
+
+        const isCollidingWithGrid = (grid, position, bubbleSize) => {
+            const gridX = Math.floor(position.x / gridSize);
+            const gridY = Math.floor(position.y / gridSize);
+            const gridWidth = Math.ceil(bubbleSize / gridSize);
+            const gridHeight = Math.ceil(bubbleSize / gridSize);
+
+            for (let x = gridX; x < gridX + gridWidth; x++) {
+                for (let y = gridY; y < gridY + gridHeight; y++) {
+                    if (grid[x] && grid[x][y]) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        };
+
+        const markGridOccupied = (grid, position, bubbleSize) => {
+            const gridX = Math.floor(position.x / gridSize);
+            const gridY = Math.floor(position.y / gridSize);
+            const gridWidth = Math.ceil(bubbleSize / gridSize);
+            const gridHeight = Math.ceil(bubbleSize / gridSize);
+
+            for (let x = gridX; x < gridX + gridWidth; x++) {
+                for (let y = gridY; y < gridY + gridHeight; y++) {
+                    if (grid[x]) {
+                        grid[x][y] = true;
+                    }
+                }
+            }
         };
 
         // Call the function to generate positions when the component mounts
@@ -84,7 +118,7 @@ const Bubble = () => {
 
     return (
         <div
-            className={'Wrapper'}
+            className={S.Wrapper} // Using the CSS module class name
             ref={wrapper}
             style={{ height: '500px', width: '100%', position: 'relative', overflow: 'hidden' }}
         >
@@ -97,13 +131,9 @@ const Bubble = () => {
                         width: `${position.size}px`,
                         height: `${position.size}px`,
                         position: 'absolute',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
                     }}
-                    className={`${S.bubble} ${getClassName(i)}`}
+                    className={`${S.bubble} ${getClassName(i)}`} // Using the CSS module class name
                 >
-
                     <img
                         height={position.size * 0.6} // Adjust the image size relative to the bubble size
                         width={position.size * 0.6} // Adjust the image size relative to the bubble size
@@ -111,7 +141,6 @@ const Bubble = () => {
                         alt="logo"
                         style={{ objectFit: 'contain' }} // Make the image fit within the container
                     />
-
                 </div>
             ))}
         </div>
