2c13307ef9f26bcdc696e7ca52ec106d391e357e
Andrei Sabou <andrei.sabou@metro.digital>
2023-07-30 20:38:30 +0300

    Highcharts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
-import ThemeContext from "./context/Context";
 import Bubble from "./components/Bubble";
-
+import Chart from "./components/Highcharts";
+import Data from "./mvxData.json";
 function App() {
-    const [theme, setTheme] = useState();
+
+    const series = Data.reduce((acc, crt) => {
+        if (Number(crt.price_change_percentage) > 0) {
+            acc.green.push(
+                {
+                    name: crt.token_id,
+                    value: Number(parseFloat(crt.price_change_percentage).toFixed(2)),
+                    marker: {
+                        symbol: `url(https://media.elrond.com/tokens/asset/${crt.token_id}/logo.svg)`,
+                        width: '',
+                        radius: '50%',
+                        height: '',
+                        lineColor: 'rgb(17,255,0)',
+                        lineWidth: 5,
+                        clip: true,
+                        fillColor: 'rgb(17,255,0)'
+                    }
+                }
+            )
+        } else {
+            acc.red.push(
+                {
+                    name: crt.token_id,
+                    value: Math.abs(Number(parseFloat(crt.price_change_percentage).toFixed(2))),
+                    marker: {
+                        symbol: `url(https://media.elrond.com/tokens/asset/${crt.token_id}/logo.svg)`,
+                        width: 16,
+                        radius: 50,
+                        height: 16,
+                        lineColor: 'rgb(149,58,59)',
+                        lineWidth: 5,
+                        clip: true,
+                    },
+                    // marker: {
+                    //     symbol: `url(https://media.elrond.com/tokens/asset/${crt.token_id}/logo.svg)`,
+                    //     width: 16,
+                    //     height: 16
+                    // }
+                }
+            )
+        }
+
+        return acc
+    }, {red: [], green: []});
+
+    console.log(series);
+
+    const dataChart = [
+        {
+            name: "Worse Rank",
+            data: series.red,
+            color: "rgb(149,58,59)"
+        },
+        {
+            name: "Green",
+            data: series.green,
+            color: "rgb(23,85,63)"
+        },
+    ]
+
+    console.log("data chartL ", dataChart);
 
     return (
         <div className="App">
             <header className="App-header">
-                <ThemeContext.Provider value={theme}>
                     {/*<img src={logo} className="App-logo" alt="logo"/>*/}
                     <Bubble/>
-                </ThemeContext.Provider>
+                    <Chart dataChart={dataChart}/>
             </header>
         </div>
     );
---
+++ src/components/Highcharts.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+require("highcharts/modules/exporting")(Highcharts);
+require("highcharts/highcharts-more")(Highcharts);
+
+const Chart = ({dataChart}) => {
+
+    const applyBoxShadowToBubbles = (chart) => {
+        const bubbles = chart.series[0]?.points;
+        if (bubbles) {
+            bubbles.forEach(bubble => {
+                const element = bubble.graphic && bubble.graphic.element;
+                if (element) {
+                    element.style.boxShadow = `
+            0 -0.06em 0.1em hsl(120, 90%, 100%) inset,
+            0 -0.15em 0.4em hsl(120, 90%, 45%) inset,
+            0 0.05em 0.05em hsl(120, 90%, 45%) inset,
+            0.05em 0 0.1em hsl(120, 90%, 100%) inset,
+            -0.05em 0 0.1em hsl(120, 90%, 100%) inset,
+            0 0.1em 0.4em hsl(120, 90%, 60%) inset
+          `;
+                }
+            });
+        }
+    };
+
+    const options = {
+        chart: {
+            type: 'packedbubble',
+            height: '800px',
+            margin: 0,
+            styledMode: false,
+            backgroundColor: '#282c34',
+        },
+        title: {
+            text: 'MVX Bubblechart',
+            style: {
+                fontSize: '20px'
+            }
+        },
+        credits: {
+            enabled: false
+        },
+        legend: {enabled: false},
+        exporting: {enabled: false},
+        series: dataChart,
+        plotOptions: {
+            packedbubble: {
+                minSize: 30,
+                maxSize: '250%',
+                dataLabels: {
+                    backgroundColor:undefined,
+                    // borderColor: "#FFFF",
+                    // borderRadius:5,
+                    // borderWidth:2,
+                    enabled: true,
+                    useHTML: true,
+                    align: 'center',
+                    verticalAlign: 'middle',
+                    formatter: function () {
+                        console.log(this.point.id);
+                        const percentage = this.point.y > 1 ? this.point.y + '%': '';
+                        const classNameLabel = this.point.y > 1 ? 'bubbleLabel' : '';
+                        const options = this.point.options
+                        const heightDiv = (this.point.marker.height) + 'px';
+                        const widthDiv = (this.point.marker.width) + 'px';
+
+                        const url = ` https://media.elrond.com/tokens/asset/${options.name}/logo.svg`;
+                        return `<div class="wrapperDataLabels">` +
+                                // `<img  class="imgClass"  src=${url} alt=""/>`+
+                                `<div class="${classNameLabel}">${percentage}</div>` +
+                            '<div>'
+                    },
+
+                    style: {
+                        fontSize: '10px',
+                        boxShadow: '0 -0.06em 0.1em hsl(120,90%,100%) inset, 0 -0.15em 0.4em hsl(120,90%,45%) inset, 0 0.05em 0.05em hsl(120,90%,45%) inset, 0.05em 0 0.1em hsl(120,90%,100%) inset, -0.05em 0 0.1em hsl(120,90%,100%) inset, 0 0.1em 0.4em hsl(120,90%,60%) inset',
+                    }
+                },
+            }
+        },
+        tooltip: {
+            useHTML: true,
+            outside: true,
+            enabled: false,
+            fontSize: '',
+            shape: "square",
+            style: {
+                fontSize: '20px'
+            },
+            formatter: function () {
+                return '<div>test</div>'
+            }
+        }
+    };
+
+    return (
+        <div>
+            <HighchartsReact width={'100%'} highcharts={Highcharts} options={options}/>
+        </div>
+    )
+
+}
+
+export default Chart;
